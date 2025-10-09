27c3489817f26abf1ddad118255c57bebf06f269
Calvin Low <calsaviour@gmail.com>
2025-10-09 11:49:46 +0800

    Add consumer and producer to use queue

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,6 +1,6 @@
-import { getDestinationForCountry } from '@/helpers/route.ops';
-import { getLink } from '@repo/data-ops/queries/links';
+import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/route.ops';
 import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
+import { LinkClickMessageType } from '@repo/data-ops/zod-schema/queue';
 
 import { Hono } from 'hono';
 
@@ -8,8 +8,7 @@ export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
     const id = c.req.param('id');
-
-    const linkInfo = await getLink(id)
+    const linkInfo = await getRoutingDestinations(c.env, id)
     if (!linkInfo) {
         return c.json({ error: 'Destination not found' }, 404)
     }
@@ -23,6 +22,21 @@ App.get('/:id', async (c) => {
     console.log(headers)
 
     const destination = getDestinationForCountry(linkInfo, headers.country)
+    const queueMessage: LinkClickMessageType = {
+        "type": "LINK_CLICK",
+        data: {
+          id: id,
+          country: headers.country,
+          destination: destination,
+          accountId: linkInfo.accountId,
+          latitude: headers.latitude,
+          longitude: headers.longitude,
+          timestamp: new Date().toISOString()
+        }
+      }
+      c.executionCtx.waitUntil(
+        c.env.QUEUE.send(queueMessage)
+      )
 
     return c.redirect(destination)
 })
